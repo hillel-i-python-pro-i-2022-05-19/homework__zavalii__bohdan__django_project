ab1d3ec8dbe79cd4b4444060a219e356583f098f
EconLQ <zavaliybogdan@gmail.com>
2022-06-23 15:07:04 +0300

    🧠 developed better logic of generating user

--- user_generator/views.py
+++ user_generator/views.py
@@ -1,14 +1,13 @@
 from django.http import HttpResponse
 
 from user_generator.services import generator_of_users
-from user_generator.utils import generate_users
 
 
 def generate_users_view(request, users_count: int = 100) -> HttpResponse:
     output_response = ''.join(
         f"""<p>
-        {generate_users().name} - {generate_users().unique_email_address}</p>"""
-        for _ in generator_of_users(amount_of_users=users_count)
+        {user.name} - {user.unique_email_address}</p>"""
+        for user in generator_of_users(amount_of_users=users_count)
     )
     return HttpResponse(
         f"""<p>Number of users to generate: {users_count}</p>
