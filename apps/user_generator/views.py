a2134900e20af245fec2bbb9ca3020e820df251c
EconLQ <zavaliybogdan@gmail.com>
2022-06-25 00:01:59 +0300

    ✨ Changed output response logic

--- apps/user_generator/views.py
+++ apps/user_generator/views.py
@@ -1,15 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
 
 from apps.user_generator.services import generator_of_users
 
 
-def generate_users_view(request, users_count: int = 100) -> HttpResponse:
-    output_response = ''.join(
-        f"""<p>
-        {user.name} - {user.unique_email_address}</p>"""
-        for user in generator_of_users(amount_of_users=users_count)
-    )
-    return HttpResponse(
-        f"""<p>Number of users to generate: {users_count}</p>
-        <p>{output_response}</p>"""
+def generate_users_view(request: HttpRequest,
+                        users_count: int = 100) -> HttpResponse:
+    output_response = generator_of_users(amount_of_users=users_count)
+    return render(
+        request, 'user_generator/show_users.html',
+        {"output_response": output_response}
     )
