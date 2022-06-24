cbe7012a80b47de97bf0898b4d30f58c78538f2f
EconLQ <zavaliybogdan@gmail.com>
2022-06-25 00:00:20 +0300

    ➕ Added routes from apps: user-generator, contacts, base

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.user_generator.urls')),
+    path('user-generator/', include('apps.user_generator.urls')),
+    path('humans/', include('apps.contacts.urls')),
+    path('', include('apps.base.urls')),
+
 ]
