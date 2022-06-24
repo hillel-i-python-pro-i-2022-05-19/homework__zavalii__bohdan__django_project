bd16d1d23f287b7a4bd59f76aaa104f2b15736c8
EconLQ <zavaliybogdan@gmail.com>
2022-06-24 23:59:36 +0300

    ➕ Added routes to views

--- apps/user_generator/urls.py
+++ apps/user_generator/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from apps.user_generator.views import generate_users_view
 
+app_name = 'user_generator'
 urlpatterns = [
-    path('', generate_users_view),
+    path('', generate_users_view, name='index'),
     path('<int:users_count>', generate_users_view)
 ]
