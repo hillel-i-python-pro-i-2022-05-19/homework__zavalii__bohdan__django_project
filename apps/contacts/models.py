d1305a1945544d9a00b893a39d7c1e192ecf3511
EconLQ <zavaliybogdan@gmail.com>
2022-06-26 20:44:20 +0300

    🐳 Added static/img folder with favicon

--- apps/contacts/models.py
+++ apps/contacts/models.py
@@ -5,13 +5,13 @@ from django.db import models
 class Contact(models.Model):
     contact_name = models.CharField('Name', max_length=200,
                                     help_text="It is the name of the person")
-    phone_regex = RegexValidator(
+    _phone_regex = RegexValidator(
         regex=r'^\+?1?\d{9,15}$',
         message="Phone number must be entered in the format: '+999999999'. "
                 "Up to 15 digits allowed."
     )
     phone_value = models.CharField(
-        'Phone number', validators=[phone_regex], max_length=17, blank=True,
+        'Phone number', validators=[_phone_regex], max_length=17, blank=True,
         help_text="The contact person phone number"
     )
 
