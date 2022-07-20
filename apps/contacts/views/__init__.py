e8b707e4c934c56108b7fa92fb52f3af81c4e654
EconLQ <zavaliybogdan@gmail.com>
2022-07-20 20:01:31 +0300

    🏴 Apply Black 2nd time

--- apps/contacts/views/__init__.py
+++ apps/contacts/views/__init__.py
@@ -1,3 +1,4 @@
+# flake8: noqa: F401
 from apps.contacts.views.contact_create_view import ContactCreateView
 from apps.contacts.views.contact_delete_view import DeleteContactsView
 from apps.contacts.views.contact_update_view import ContactUpdateView
