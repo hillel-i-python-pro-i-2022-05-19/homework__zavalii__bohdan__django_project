a82292a92a17cdb576b645f9cc2cb89e7d7fa41d
EconLQ <zavaliybogdan@gmail.com>
2022-07-20 19:51:53 +0300

    🏴 Apply Black 1st time

--- apps/user_generator/urls.py
+++ apps/user_generator/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from apps.user_generator.views import GenerateUsersView
 
-app_name = 'user_generator'
+app_name = "user_generator"
 
 urlpatterns = [
-    path('', GenerateUsersView.as_view(), name='index'),
-    path('<int:users_count>', GenerateUsersView.as_view())
+    path("", GenerateUsersView.as_view(), name="index"),
+    path("<int:users_count>", GenerateUsersView.as_view()),
 ]
